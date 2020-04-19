output_lengths, from models, is wrong types. Using old computation instead, for now.

jasper.py
@@ -446,7 +446,7 @@ class Jasper(nn.Module):
         self.jasper_encoder.apply(init_weights)
         self.final_layer.apply(init_weights)
 
-        self.scaling_factor = np.prod([block.mconv[0].conv.stride[0] for block in blocks[:mid_layers]])
+        self.scaling_factor = int(np.prod([block.mconv[0].conv.stride[0] for block in blocks[:mid_layers]]))
         
     def forward(self,xs,input_lengths):
         '''
@@ -461,7 +461,7 @@ class Jasper(nn.Module):
         else:
             jasper_res = F.softmax(jasper_res,dim=-1)
         assert not (jasper_res != jasper_res).any()  # is there any NAN in result?
-        return jasper_res,outout_lengths # [Batches X Labels X Time (padded to max)], [Batches]
+        return jasper_res, outout_lengths # [Batches X Labels X Time (padded to max)], [Batches]
 
     @classmethod
     def load_model(cls,path):

wav2letter.py
@@ -93,7 +93,7 @@ class Wav2Letter(nn.Module):
         strides = []
         for module in self.conv1ds.children():
             strides.append(module.conv1.stride[0])
-        self.scaling_factor = np.prod(strides)
+        self.scaling_factor = int(np.prod(strides))
 
     def forward(self, x, input_lengths=None):
         x = self.conv1ds(x)
@@ -102,7 +102,7 @@ class Wav2Letter(nn.Module):
             x = F.log_softmax(x,dim=-1)
         else:
             x = F.softmax(x,dim=-1)
-        if input_lengths:
+        if input_lengths is not None:
             output_lengths = [l // self.scaling_factor for l in input_lengths]
         else:
             output_lengths = None